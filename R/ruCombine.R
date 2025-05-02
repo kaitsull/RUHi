073fb55baec9673aa82014065769c0be29c68a0c
Kaitlin Sullivan <54858629+kaitsull@users.noreply.github.com>
2025-05-02 07:04:16 -0700

    Update ruCombine.R

--- R/ruCombine.R
+++ R/ruCombine.R
@@ -10,40 +10,23 @@
 #'
 #' @export
 #' @import dplyr
-ruCombine <- function(dfs) {
-  #if the data is 2+ dataframes
-  if(length(dfs)>1){
+ruCombine <- function (dfs) 
+{
+  if (length(dfs) > 1) {
     print("Combining tables...")
-
-    #save the last id number of first df
-    cur <- dfs[1][[1]]
-    cur <- dplyr::mutate(cur, id=1:nrow(cur))
-
-    #subsequent
-    nxt <- dfs[2][[1]]
-
-    #last id #
-    lst <- nrow(cur)
-
-    #iterate through
-    for(i in 1:length(dfs)-1){
-      print(paste("Table: ", i+1, sep=""))
-
-      #change id numbers for subsequent round
-      nxt <- dfs[i+1][[1]]
-      nxt <- dplyr::mutate(nxt, id=1:nrow(nxt),
-                           id=id+lst)
-
-      #bind tables
+    cur <- dfs[[1]]
+    print(paste("Table 1 - nrow =", nrow(cur)))
+    for (i in 2:length(dfs)) {
+      print(i)
+      nxt <- dfs[[i]]
+      print(paste("Table", i, "- nrow =", nrow(nxt)))
       cur <- rbind(cur, nxt)
-
-      #update last number for ids
-      lst <- nrow(cur)
     }
-    #return table
+    cur <- dplyr::mutate(cur, id = 1:nrow(cur))
+    print(paste("Combined length =", nrow(cur)))
     cur
   }
-  else{
-    warning("This function requires a vector of dataframes with length >= 2.")
+  else {
+    warning("This function requires a list of data frames with length >= 2.")
   }
 }
